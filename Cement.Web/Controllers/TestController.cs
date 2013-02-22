b6a86e2c00404dbcf5844942290fcdb8477ce700
Konst <kostiantyn.kopytov@gmail.com>
2013-02-22 15:22:11 +0200

    angular checks 1

---
+++ Cement.Web/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestController.cs" company="">
+//   Cement (c) 2013
+// </copyright>
+// <summary>
+//   The test controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Cement.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The test controller.
+    /// </summary>
+    public class TestController : Controller
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Index()
+        {
+            var model = new { Layout = "_Basic" };
+            return this.View(model);
+        }
+
+        #endregion
+    }
+}
