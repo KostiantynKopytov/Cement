ed069b23ec63939aad6869fe810e5fbc82339abc
anton.pavlov <anton.vp@me.com>
2013-02-24 11:08:34 +0200

    Layout handling

--- Cement.Web/Controllers/TemplatesController.cs
+++ Cement.Web/Controllers/TemplatesController.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Cement.Web.Controllers
 {
     public class TemplatesController : Controller
     {
-        //
-        // GET: /Templates/
-
-
-
         public ActionResult Index(string page)
         {
             return View(page);
         }
-
-
     }
 }
