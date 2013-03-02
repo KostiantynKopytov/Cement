d6494d76035abac66ec173b3140100469e22e629
konst <kostiantyn.kopytov@gmail.com>
2013-03-02 19:26:44 +0200

    d&d moved to placeholder

--- concrete/core/directives/placeholder.directive.js
+++ concrete/core/directives/placeholder.directive.js
@@ -10,6 +10,52 @@
             link: function (scope, element, attrs) {
                 var links = $("link[data-usage='0']");
                 links.remove();
+                
+                var item = null;
+                var receiver = null;
+                var receiverIndex = 0;
+                var sender = null;
+                var senderIndex = 0;
+                $(element).sortable({
+                    connectWith: '[ct-placeholder]',
+                    cursor: 'move',
+                    cursorAt: { top: 0, left: 0 },
+                    forcePlaceholderSize: true,
+                    forceHelperSize: true,
+                    tolerance: "pointer",
+                    distance: 10,
+                    over: function (event, ui) {
+                        var parent = ui.placeholder.parent();
+                        receiver = parent.data('$scope');
+                        receiverIndex = ui.placeholder.index();
+                    },
+                    activate: function (event, ui) {
+                        $("[ct-placeholder]").sortable("refresh");
+                        ui.placeholder && ui.placeholder.attr('style', '').addClass('placeholder-targetplace').html(ui.item.html());
+                        var parent = ui.placeholder.parent();
+                        receiver = parent.data('$scope');
+                        receiverIndex = ui.placeholder.index();
+                    },
+                    receive: function (event, ui) {
+                        item = ui.item.data('$scope');
+                        sender = ui.sender.data('$scope');
+                        senderIndex = sender.widgets.indexOf(item.widget);
+                        console.log('beforeStop', sender.widgets, senderIndex, receiver.widgets, receiverIndex);
+
+                        sender.widgets.splice(senderIndex, 1);
+                        receiver.widgets = receiver.widgets || [];
+                        receiver.widgets.splice(receiverIndex, 0, item.widget);
+                        $scope.$apply();
+                    }
+                }).droppable({
+                    greedy: true,
+                    activeClass: 'placeholder-active'
+                }).disableSelection();
+
+                scope.$on('$destory', function() {
+                    $(element).sortable('destroy');
+                    $(element).droppable('destroy');
+                });
             }
         };
     }]);
--- concrete/portal/controllers/app.controller.js
+++ concrete/portal/controllers/app.controller.js
@@ -19,53 +19,5 @@
         $scope.changeTitle = function() {
             $scope.page.placeholders.left[0].settings.title += " vasya";
         };
-        
-        $scope.$watch(function () {
-            return $('[ct-placeholder], [ct-widget]').length;
-        }, function () {
-            var item = null;
-            var receiver = null;
-            var receiverIndex = 0;
-            var sender = null;
-            var senderIndex = 0;
-            $('.droppable.ui-sortable').sortable('destroy');
-            $('.droppable.ui-droppable').droppable('destroy');
-            $('.droppable').sortable({
-                connectWith: '.droppable',
-                cursor: 'move',
-                cursorAt: { top: 0, left: 0 },
-                forcePlaceholderSize: true,
-                forceHelperSize: true,
-                tolerance: "pointer",
-                distance: 10,
-                over: function(event, ui) {
-                    var parent = ui.placeholder.parent();
-                    receiver = parent.data('$scope');
-                    receiverIndex = ui.placeholder.index();
-                },
-                activate: function (event, ui) {
-                    $(".droppable").sortable("refresh");
-                    ui.placeholder && ui.placeholder.attr('style', '').addClass('placeholder-targetplace').html(ui.item.html());
-                    var parent = ui.placeholder.parent();
-                    receiver = parent.data('$scope');
-                    receiverIndex = ui.placeholder.index();
-                },
-                receive: function (event, ui) {
-                    item = ui.item.data('$scope');
-                    sender = ui.sender.data('$scope');
-                    senderIndex = sender.widgets.indexOf(item.widget);
-                    console.log('beforeStop', sender.widgets, senderIndex, receiver.widgets, receiverIndex);
-
-                    sender.widgets.splice(senderIndex, 1);
-                    receiver.widgets = receiver.widgets || [];
-                    receiver.widgets.splice(receiverIndex, 0, item.widget);
-                    $scope.$apply();
-                }
-            }).droppable({
-                greedy: true,
-                activeClass: 'placeholder-active'
-            }).disableSelection();
-        });
-
     }]);
 });
