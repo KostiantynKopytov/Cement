a48ebe49c0ef2cc2156d128f715bf93b020af8aa
anton.pavlov <anton.vp@me.com>
2013-03-05 01:25:38 +0200

    refactoring, rich text

---
+++ concrete/core/directives/richtext.directive.js
@@ -0,0 +1,11 @@
+﻿define(['jquery', 'module!core', 'libs/markitup/sets/default/set', 'jquery-markitup', 'css!libs/markitup/sets/default/style'], function ($, module, markitupSettings) {
+    module.directive('ctRichText', function () {
+        return {
+            restrict: 'A',
+            scope: {},
+            link: function ($scope, element, $attrs) {
+                $(element).markItUp(markitupSettings);
+            }
+        };
+    });
+});
