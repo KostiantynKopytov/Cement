63f278f26411b404c544d93004325c16e470520d
anton.pavlov <anton.vp@me.com>
2013-03-05 01:49:44 +0200

    ric htext

--- concrete/core/directives/richtext.directive.js
+++ concrete/core/directives/richtext.directive.js
@@ -1,9 +1,11 @@
 ﻿define(['jquery', 'module!core', 'libs/markitup/sets/default/set', 'jquery-markitup', 'css!libs/markitup/sets/default/style'], function ($, module, markitupSettings) {
     module.directive('ctRichText', function () {
         return {
-            restrict: 'A',
-            scope: {},
-            link: function ($scope, element, $attrs) {
+            restrict: 'AC',
+            link: function (scope, element, $attrs) {
+                markitupSettings.afterInsert = function () {
+                    //scope.$root.$apply();
+                };
                 $(element).markItUp(markitupSettings);
             }
         };
