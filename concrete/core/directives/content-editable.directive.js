87ff72d2a490127bfda637cf8737a6e0671afbc8
konst <kostiantyn.kopytov@gmail.com>
2013-03-07 13:22:05 +0200

    content editable directive

---
+++ concrete/core/directives/content-editable.directive.js
@@ -0,0 +1,32 @@
+﻿define(['jquery', 'module!core'], function($, module) {
+    module.directive('ctContentEditable', [function() {
+        return {
+            restrict: 'A',
+            scope: {
+                data: '=ctContentEditable'
+            },
+            compile: function(tElement, tAttrs) {
+                tElement.attr('contentEditable', 'true');
+
+                return function (scope, element, attrs) {
+                    scope.$parent.$watch('data', function(data, oldData) {
+                        if ($(element).html() != data) {
+                            $(element).html(data);
+                        }
+                    });
+
+                    $(element).on('blur keyup paste', function () {
+                        var $this = $(this);
+                        if (scope.data !== $this.html()) {
+                            $this.trigger('change');
+
+                            scope.data = $this.html();
+                            scope.$apply();
+                        }
+                        return $this;
+                    });
+                };
+            },
+        };
+    }]);
+});
