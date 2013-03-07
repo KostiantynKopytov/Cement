69985088e78c43669a40b2a773702b7109de08aa
konst <kostiantyn.kopytov@gmail.com>
2013-03-07 23:47:14 +0200

    bug fix

--- concrete/core/directives/require-css.directive.js
+++ concrete/core/directives/require-css.directive.js
@@ -14,6 +14,8 @@
                         scope.$apply();
 
                         scope.$on('$destroy', function () {
+                            var selector = String.Format("style[data-href='{0}']", name);
+                            var link = $(selector);
                             link.attr('data-usage', (link.attr('data-usage') || 0) - 1);
                         });
                     });
