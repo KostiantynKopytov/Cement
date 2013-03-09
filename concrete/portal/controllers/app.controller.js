88287c598c6f3e3a0018a026487d2d74c9f274d5
anton.pavlov <anton.vp@me.com>
2013-03-09 14:59:37 +0200

    lalala

--- concrete/portal/controllers/app.controller.js
+++ concrete/portal/controllers/app.controller.js
@@ -1,8 +1,9 @@
 ﻿define(['module!portal', 'jquery', 'extensions'], function(module, $, ext) {
-    module.controller('AppController', ["$scope", "$location", "coreService", function($scope, $location, coreService) {
+    module.controller('AppController', ["$scope", "$location", "coreService", function ($scope, $location, coreService) {
+
         $scope.$on("$locationChangeSuccess", function () {
             var path = $location.path();
-            if (path === '/') {
+            if (path === '' || path === '/') {
                 $location.path('/home');
             } else {
                 coreService.getPage(path).success(function (data) {
@@ -11,16 +12,6 @@
                     // TODO: handle this
                 });
             }
-
-//            setTimeout(function () {
-//                if (path === '/home') {
-//                    $location.path('/vasya');
-//                }
-//                if (path === '/vasya') {
-//                    $location.path('/home');
-//                }
-//                $scope.$apply();
-//            }, 500);
 
             // cleanup css
             var links = $("style[data-usage='0']");
