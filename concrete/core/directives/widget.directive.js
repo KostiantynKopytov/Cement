7ac47b8a3ff2b333e0e8f4bd0091ad02591fe789
konst <kostiantyn.kopytov@gmail.com>
2013-03-15 01:39:45 +0200

    editor rework

--- concrete/core/directives/widget.directive.js
+++ concrete/core/directives/widget.directive.js
@@ -6,31 +6,46 @@
                 type: '@ctWidget',
                 data: '='
             },
-            compile: function (tElement, tAttrs) {
-                console.log(tElement);
+            link: function(scope, element) {
+                scope.$watch('type', function(type) {
+                    var editorUrl = String.Format("/core/widgets/{0}/{0}.editor.html", scope.type);
 
-                return function(scope, element) {
-                    scope.$watch('type', function(type) {
-                        var editorUrl = String.Format("/core/widgets/{0}/{0}.editor.html", scope.type);
-                        tElement.append($('<div ng-include />').attr('src', 'editor.url'));
-                        var jqEditor = null;
-                        var jqWidget = $('<div ct-' + type + '="data"/>');
+                    var jqWidget = $('<div><div ct-' + type + '="data"/></div>');
 
-                        $.ajax({
-                            url: editorUrl,
-                            type: 'HEAD'
-                        }).done(function() {
-                            jqEditor = $('<div />').attr('ct-editor', editorUrl).addClass('editable');
-                            jqEditor.append(jqWidget);
-                        }).fail(function() {
+                    $.ajax({
+                        url: editorUrl,
+                        type: 'HEAD'
+                    }).done(function() {
+                        element.addClass('editable');
 
-                        }).always(function() {
-                            var compiled = $compile(jqEditor || jqWidget)(scope);
-                            element.html('').append(compiled);
+                        var editButton = $('<button class="edit-button btn btn-mini" type="button" />').prepend($('<i class="icon-wrench"/>'));
+                        jqWidget.append(editButton);
+                        jqWidget.append($('<div ng-include />').attr('src', 'editor.url'));
+
+                        editButton.on('click', function() {
+                            console.log('click', scope);
+                            scope.editor = {
+                                data: ext.cleanClone(scope.data),
+                                url: editorUrl,
+                                ok: function() {
+                                    scope.data = ext.extend(scope.data, scope.editor.data);
+                                    delete scope.editor;
+                                },
+                                cancel: function() {
+                                    delete scope.editor;
+                                }
+                            };
                             scope.$apply();
                         });
+
+                    }).fail(function() {
+
+                    }).always(function() {
+                        var compiled = $compile(jqWidget.children())(scope);
+                        element.html('').append(compiled);
+                        scope.$apply();
                     });
-                };
+                });
             }
         };
     }]);
---
+++ concrete/core/widgets/page/page.widget.js
@@ -0,0 +1,12 @@
+﻿define(['module!core'], function(module) {
+    module.directive('ctPage', [function() {
+        return {
+            templateUrl: '/core/widgets/page/page.widget.html',
+            replace: true,
+            restrict: 'A',
+            scope: {
+                data: '=ctPage'
+            }
+        };
+    }]);
+});
