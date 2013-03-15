f584f41067ad7025db6ea13f4d7e9e3f09cf99c6
anton.pavlov <anton.vp@me.com>
2013-03-15 15:30:58 +0200

    editor

--- concrete/core/directives/editor.directive.js
+++ concrete/core/directives/editor.directive.js
@@ -9,18 +9,22 @@
             compile: function (tElement, tAttrs) {
                 tElement.append($('<div ng-include />').attr('src', 'src'));
                 return function (scope, element) {
-                    scope.editor = {
-                        data: ext.cleanClone(scope.data),
-                        ok: function () {
-                            scope.data = ext.extend(scope.data, scope.editor.data);
-                            scope.src = null;
-                            delete scope.editor;
-                        },
-                        cancel: function () {
-                            scope.src = null;
-                            delete scope.editor;
+                    scope.$watch('src', function (src) {
+                        if (src) {
+                            scope.editor = {
+                                data: ext.cleanClone(scope.data),
+                                ok: function () {
+                                    scope.data = ext.extend(scope.data, scope.editor.data);
+                                    scope.src = null;
+                                    delete scope.editor;
+                                },
+                                cancel: function () {
+                                    scope.src = null;
+                                    delete scope.editor;
+                                }
+                            };
                         }
-                    };
+                    });
                 };
             }
         };
